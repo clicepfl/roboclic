c39385c0a33314be5eb685ef52baffc19071032a
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-12-21 11:47:17 +0100

    refactor: remove json config

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,8 @@
 use envconfig::Envconfig;
-use serde::Deserialize;
-use std::{collections::HashMap, fs::File, io::Read, sync::OnceLock};
-
-#[derive(Deserialize)]
-struct JsonConfig {
-    committee: Vec<String>,
-    access_control: HashMap<String, Vec<i64>>,
-}
+use std::sync::OnceLock;
 
 #[derive(Envconfig)]
-pub struct EnvConfig {
+pub struct Config {
     #[envconfig(from = "BOT_TOKEN")]
     pub bot_token: String,
     #[envconfig(from = "CONFIG_FILE")]
@@ -22,41 +15,7 @@ pub struct EnvConfig {
     pub admin_token: String,
 }
 
-pub struct Config {
-    pub committee: Vec<String>,
-    pub bot_token: String,
-    pub access_control: HashMap<String, Vec<i64>>,
-    pub data_dir: String,
-    pub database_url: String,
-    pub admin_token: String,
-}
-
 static CONFIG: OnceLock<Config> = OnceLock::new();
 pub fn config() -> &'static Config {
-    CONFIG.get_or_init(|| {
-        let env_config = EnvConfig::init_from_env().unwrap();
-
-        let mut json_config = String::new();
-        File::open(&env_config.config_file)
-            .unwrap_or_else(|_| panic!("Could not open config file at {}", env_config.config_file))
-            .read_to_string(&mut json_config)
-            .unwrap_or_else(|_| {
-                panic!(
-                    "Could not read from config file at {}",
-                    env_config.config_file
-                )
-            });
-
-        let json_config: JsonConfig =
-            serde_json::from_str(json_config.as_str()).expect("Could not parse config file");
-
-        Config {
-            committee: json_config.committee,
-            bot_token: env_config.bot_token,
-            access_control: json_config.access_control,
-            data_dir: env_config.data_dir,
-            database_url: env_config.database_url,
-            admin_token: env_config.admin_token,
-        }
-    })
+    CONFIG.get_or_init(|| Config::init_from_env().unwrap())
 }
